be5e04fae9b781c3b52909a7a5c3125569543a4f
mygetzu <mbenypangestu@gmail.com>
2020-04-20 13:35:49 +0700

    bin sh developemnet

--- bin/run.development.sh
+++ bin/run.development.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-cd ~/projects/holly-translator-yandex
+git add .
+git commit -m "Update"
 git pull origin master
 ps aux | grep 'main.py' | grep -v grep | awk '{print $2}' | xargs kill
 nohup python3 main.py &
